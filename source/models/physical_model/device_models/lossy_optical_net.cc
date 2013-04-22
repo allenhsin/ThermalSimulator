ae17b5e56de7e795cbec95c37083e445c2a543f5
Yu-Hsin Chen <yhchen@mit.edu>
2013-04-22 18:33:02 -0400

    updates

--- source/models/physical_model/device_models/lossy_optical_net.cc
+++ source/models/physical_model/device_models/lossy_optical_net.cc
@@ -75,6 +75,36 @@ namespace Thermal
         }
     }
 
+    void LossyOpticalNet::initializeMonitoring()
+    {
+        unsigned int number_wavelength = 0;
+        unsigned int i = 0;
+
+        for(map<string, FILE*>::iterator it = _monitored_device_ports.begin(); it != _monitored_device_ports.end(); ++it)
+        {
+            number_wavelength = getPort(it->first)->getPortPropertySize("wavelength");
+            
+            for(i=0; i<number_wavelength; ++i)
+                fprintf(it->second, "%.12f ", getPort(it->first)->getPortPropertyValueByIndex("wavelength", i) );
+            fprintf(it->second, "\n\n\n");
+        }
+    }
+
+    void LossyOpticalNet::printMonitoredResult()
+    {
+        unsigned int number_wavelength = 0;
+        unsigned int i = 0;
+
+        for(map<string, FILE*>::iterator it = _monitored_device_ports.begin(); it != _monitored_device_ports.end(); ++it)
+        {
+            number_wavelength = getPort(it->first)->getPortPropertySize("power");
+                
+            for(i=0; i<number_wavelength; ++i)
+                fprintf(it->second, "%.9f ", getPort(it->first)->getPortPropertyValueByIndex("power", i) );
+            fprintf(it->second, "\n");
+        }
+    }
+
     void LossyOpticalNet::printDefinition(FILE* device_list_file)
     {
         DeviceModel::printDefinition(device_list_file);
